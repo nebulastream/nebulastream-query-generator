9fb3a06aec85be92d646d96c728be2064e3d6bff
Ankit Chaudhary <ankit.chaudhary@tu-berlin.de>
2021-09-05 22:58:52 +0200

    added startegy to generate equivalent and distinct union operators

--- operator_generator_strategies/distinct_operator_strategies/distinct_union_strategy.py
+++ operator_generator_strategies/distinct_operator_strategies/distinct_union_strategy.py
@@ -1,22 +1,18 @@
 from typing import List
 
+from utils.utils import shuffle_list
 from operator_generator_strategies.base_generator_strategy import BaseGeneratorStrategy
-from operators.filter_operator import FilterOperator
-from utils.contracts import Schema, Operator, LogicalExpression, FieldAccessExpression, \
-    ConstantExpression, LogicalOperators
-from utils.utils import random_list_element, random_int_between
+from operators.union_operator import UnionOperator
+from query_generator.query import Query
+from utils.contracts import Operator
 
 
-class DistinctFilterGeneratorStrategy(BaseGeneratorStrategy):
-    def __init__(self, max_number_of_predicates: int = 1):
+class DistinctUnionGeneratorStrategy(BaseGeneratorStrategy):
+    def __init__(self):
         super().__init__()
-        self._max_number_of_predicates = max_number_of_predicates
 
-    def generate(self, schema: Schema) -> List[Operator]:
-        _, field = random_list_element(schema.get_numerical_fields())
-        constant = random_int_between(1, 10000)
-        _, operator = random_list_element(
-            [LogicalOperators.lt, LogicalOperators.gt, LogicalOperators.lte, LogicalOperators.gte])
-        filter_operator = FilterOperator(
-            LogicalExpression(FieldAccessExpression(field), ConstantExpression(constant), operator), schema)
-        return [filter_operator]
+    def generate(self, subQueries: List[Query]) -> List[Operator]:
+        shuffledSubQueries = shuffle_list(subQueries)
+        schema = shuffledSubQueries[0].output_schema()
+        union = UnionOperator(schema=schema, leftSubQuery=shuffledSubQueries[0], rightSubQuery=shuffledSubQueries[1])
+        return [union]
--- operator_generator_strategies/equivalent_operator_strategies/union_equivalent_strategy.py
+++ operator_generator_strategies/equivalent_operator_strategies/union_equivalent_strategy.py
@@ -1,66 +1,18 @@
-from copy import deepcopy
 from typing import List
 
+from utils.utils import shuffle_list
 from operator_generator_strategies.base_generator_strategy import BaseGeneratorStrategy
-from operators.filter_operator import FilterOperator
-from operators.map_operator import MapOperator
-from utils.contracts import *
-from utils.utils import *
+from operators.union_operator import UnionOperator
+from query_generator.query import Query
+from utils.contracts import Operator
 
 
-class FilterEquivalentFilterGeneratorStrategy(BaseGeneratorStrategy):
+class UnionEquivalentUnionGeneratorStrategy(BaseGeneratorStrategy):
     def __init__(self):
-        self._equivalentFilterOperators: List[List[Operator]] = []
+        super().__init__()
 
-    def generate(self, schema: Schema) -> List[Operator]:
-        """
-        Queries with similar filters (here y is an Integer value):
-        Examples:
-        1. map(y = y*10).filter(y<31) vs map(y = y*10).filter(y<35)
-        2. filter(a*2>b*3) vs filter(a*4>b*6)
-        :param schema:
-        :return:
-        """
-        if not self._equivalentFilterOperators:
-            self.__initializeEquivalentFilters(schema)
-
-        _, filterOps = random_list_element(self._equivalentFilterOperators)
-        return filterOps
-
-    def __initializeEquivalentFilters(self, schema: Schema):
-        schemaCopy = deepcopy(schema)
-        option = random_int_between(1, 2)
-        intFields = schemaCopy.int_fields
-
-        if option == 1:
-            assignmentFieldName1 = random_field_name(intFields)
-            intFields.remove(assignmentFieldName1)
-            assignmentFieldName2 = random_field_name(intFields)
-            _, arithOperation = random_list_element(list(ArithmeticOperators))
-
-            _, logicalOperation = random_list_element(
-                [LogicalOperators.lt, LogicalOperators.gt, LogicalOperators.lte, LogicalOperators.gte])
-
-            for i in range(10):
-                contValue = random_int_between(1, 10)
-                arithExpression1 = ArithmeticExpression(FieldAccessExpression(assignmentFieldName1),
-                                                        ConstantExpression(str(contValue)), arithOperation)
-                arithExpression2 = ArithmeticExpression(FieldAccessExpression(assignmentFieldName2),
-                                                        ConstantExpression(str(contValue)), arithOperation)
-                filterOp = FilterOperator(
-                    LogicalExpression(arithExpression1, arithExpression2, logicalOperation),
-                    schema)
-                self._equivalentFilterOperators.append([filterOp])
-        else:
-
-            assignmentField = random_field_name(intFields)
-            contValue = 10
-            arithExpression = ArithmeticExpression(ConstantExpression(str(contValue)),
-                                                   FieldAccessExpression(assignmentField), ArithmeticOperators.Mul)
-            mapOp = MapOperator(FieldAssignmentExpression(FieldAccessExpression(assignmentField), arithExpression),
-                                schema)
-            for i in range(31, 39):
-                followUpFilter = FilterOperator(
-                    LogicalExpression(FieldAccessExpression(assignmentField), ConstantExpression(str(i)),
-                                      LogicalOperators.lt), schema)
-                self._equivalentFilterOperators.append([mapOp, followUpFilter])
+    def generate(self, subQueries: List[Query]) -> List[Operator]:
+        shuffledSubQueries = shuffle_list(subQueries)
+        schema = shuffledSubQueries[0].output_schema()
+        union = UnionOperator(schema=schema, leftSubQuery=shuffledSubQueries[0], rightSubQuery=shuffledSubQueries[1])
+        return [union]
